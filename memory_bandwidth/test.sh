e6ca2e22c11c4c38c972036850115acdd4f4ad69
Fan JIN <kingium@outlook.com>
2020-12-11 00:46:01 -0800

    add memory bandwidth code

---
+++ memory_bandwidth/test.sh
@@ -0,0 +1,9 @@
+gcc -O0 -o main.out main.c && chmod +x main.out
+./main.out 1
+./main.out 10
+./main.out 100
+./main.out 1000
+./main.out 10000
+./main.out 100000
+./main.out 1000000
+./main.out 10000000
