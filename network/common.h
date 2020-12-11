3c7d61768f87807e67b935963ddd75449b43162c
Fan JIN <kingium@outlook.com>
2020-12-10 21:50:46 -0800

    add network code

---
+++ network/common.h
@@ -0,0 +1,33 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+#include <assert.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+#define PORT 58001
+#define BUFFSIZE (64 * 1024)
+
+#endif
